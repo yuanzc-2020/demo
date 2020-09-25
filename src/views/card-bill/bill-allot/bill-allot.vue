9242f35f94092cade208cf237fa462cee9dd8241
maxbm <12530833@qq.com>
2020-09-25 17:09:44 +0800

    测试更改提交

--- src/views/card-bill/bill-allot/bill-allot.vue
+++ src/views/card-bill/bill-allot/bill-allot.vue
@@ -4,15 +4,15 @@
 <template>
 	<div class="search">
 		<AllotAdd
-			v-if="currView=='add'"
-			@close="currView='index'"
+			v-if="currView == 'add'"
+			@close="currView = 'index'"
 			@fatherMethod="fatherMethod"
 			@submited="submited"
 			:transferData="transferData"
 		/>
 		<AllotDetail
-			v-if="currView=='detail'"
-			@close="currView='index'"
+			v-if="currView == 'detail'"
+			@close="currView = 'index'"
 			@submited="submited"
 			:businessId="businessId"
 		/>
@@ -21,17 +21,26 @@
 			@close="currView = 'index'"
 			:traceId="businessId"
 		/>
-		<Card v-show="currView=='index'">
+		<Card v-show="currView == 'index'">
 			<Row v-show="openSearch" @keydown.enter.native="handleSearch">
-				<Form ref="searchForm" :model="searchForm" inline :label-width="100">
+				<Form
+					ref="searchForm"
+					:model="searchForm"
+					inline
+					:label-width="100"
+				>
 					<Form-item label="数据状态" prop="statusCode">
-						<Select v-model="searchForm.statusCode" style="width: 250px;">
+						<Select
+							v-model="searchForm.statusCode"
+							style="width: 250px"
+						>
 							<Option
 								v-for="item in statusCodes"
 								:value="item.statusCode"
 								:key="item.statusCode"
 								:label="item.statusName"
-							>{{ item.statusName }}</Option>
+								>{{ item.statusName }}</Option
+							>
 						</Select>
 					</Form-item>
 
@@ -42,27 +51,48 @@
 							format="yyyy-MM-dd"
 							@on-change="selectDateRange"
 							placeholder="选择起始时间"
-							style="width: 250px;"
+							style="width: 250px"
 						></DatePicker>
 					</Form-item>
-					<Form-item style="margin-left:-35px;" class="br">
-						<Button @click="handleSearch" type="primary" icon="ios-search">查询</Button>
+					<Form-item style="margin-left: -35px" class="br">
+						<Button
+							@click="handleSearch"
+							type="primary"
+							icon="ios-search"
+							>查询</Button
+						>
 						<Button @click="handleReset">重置</Button>
 					</Form-item>
 				</Form>
 			</Row>
 			<Row class="operation">
-				<Button @click="add" type="primary" icon="md-add">新增调拨单</Button>
-				<Button @click="processSubmit" type="warning" icon="md-paper-plane">流程提交</Button>
-				<Button @click="processTrace" type="warning" icon="md-git-branch">流程跟踪</Button>
+				<Button @click="add" type="primary" icon="md-add"
+					>新增调拨单</Button
+				>
+				<Button
+					@click="processSubmit"
+					type="warning"
+					icon="md-paper-plane"
+					>流程提交</Button
+				>
+				<Button
+					@click="processTrace"
+					type="warning"
+					icon="md-git-branch"
+					>流程跟踪</Button
+				>
 				<Button type="dashed" @click="getDataList">刷新数据</Button>
-				<Button type="dashed" @click="openSearch=!openSearch">{{openSearch ? "关闭搜索" : "开启搜索"}}</Button>
-				<Button type="dashed" @click="openTip=!openTip">{{openTip ? "关闭提示" : "开启提示"}}</Button>
+				<Button type="dashed" @click="openSearch = !openSearch">{{
+					openSearch ? '关闭搜索' : '开启搜索'
+				}}</Button>
+				<Button type="dashed" @click="openTip = !openTip">{{
+					openTip ? '关闭提示' : '开启提示'
+				}}</Button>
 			</Row>
 			<Row v-show="openTip">
 				<Alert show-icon>
 					已选择数据
-					<span class="select-count">{{selectCount}}</span>
+					<span class="select-count">{{ selectCount }}</span>
 					<a class="select-clear" @click="clearSelectAll">清空</a>
 				</Alert>
 			</Row>
@@ -88,7 +118,7 @@
 					:page-size="searchForm.pageSize"
 					@on-change="changePage"
 					@on-page-size-change="changePageSize"
-					:page-size-opts="[10,20,50]"
+					:page-size-opts="[10, 20, 50]"
 					size="small"
 					show-total
 					show-elevator
@@ -111,7 +141,7 @@ import {
 	getAllotList,
 	deleteAllot,
 	getAllotStatus,
-	updateAlloatCardStatus
+	updateAlloatCardStatus,
 } from '@/api/card-bill-api';
 import AllotAdd from './bill-allot-form.vue';
 import AllotDetail from './bill-allot-detail.vue';
@@ -123,7 +153,7 @@ export default {
 		AllotAdd,
 		AllotDetail,
 		ProcessStart,
-		ProcessHistoricTrace
+		ProcessHistoricTrace,
 	},
 	data() {
 		return {
@@ -142,34 +172,34 @@ export default {
 				pageSize: 10,
 				statusCode: '',
 				startDate: '',
-				endDate: ''
+				endDate: '',
 			},
 			timeForm: {
-				addTime: ''
+				addTime: '',
 			},
 			columns: [
 				{
 					type: 'index',
 					width: 60,
-					align: 'center'
+					align: 'center',
 				},
 				{
 					title: '调拨编号',
 					key: 'allotId',
 					width: 200,
-					align: 'center'
+					align: 'center',
 				},
 				{
 					title: '目的单位',
 					key: 'toCorpName',
 					minWidth: 200,
-					align: 'center'
+					align: 'center',
 				},
 				{
 					title: '数量',
 					key: 'amount',
 					minWidth: 100,
-					align: 'center'
+					align: 'center',
 				},
 				{
 					title: '单据状态',
@@ -178,25 +208,25 @@ export default {
 					minWidth: 130,
 					render: (h, params) => {
 						let text = '';
-						this.statusCodes.forEach(e => {
+						this.statusCodes.forEach((e) => {
 							if (e.statusCode == params.row.statusCode) {
 								text = e.statusName;
 							}
 						});
 						return h('div', text);
-					}
+					},
 				},
 				{
 					title: '新增人',
 					key: 'addUserName',
 					width: 150,
-					align: 'center'
+					align: 'center',
 				},
 				{
 					title: '新增时间',
 					key: 'addTime',
 					width: 180,
-					align: 'center'
+					align: 'center',
 				},
 				{
 					title: '操作',
@@ -209,17 +239,17 @@ export default {
 								'Button',
 								{
 									props: {
-										size: 'small'
+										size: 'small',
 									},
 									style: {
-										marginRight: '5px'
+										marginRight: '5px',
 									},
 
 									on: {
 										click: () => {
 											this.details(params.row);
-										}
-									}
+										},
+									},
 								},
 								'详情'
 							),
@@ -232,17 +262,17 @@ export default {
 										disabled:
 											params.row.statusCode == '1000'
 												? false
-												: true
+												: true,
 									},
 
 									style: {
-										marginRight: '5px'
+										marginRight: '5px',
 									},
 									on: {
 										click: () => {
 											this.edit(params.row);
-										}
-									}
+										},
+									},
 								},
 								'编辑'
 							),
@@ -255,26 +285,26 @@ export default {
 										disabled:
 											params.row.statusCode == '1000'
 												? false
-												: true
+												: true,
 									},
 									style: {
-										marginRight: '5px'
+										marginRight: '5px',
 									},
 									on: {
 										click: () => {
 											this.remove(params.row);
-										}
-									}
+										},
+									},
 								},
 								'删除'
-							)
+							),
 						]);
-					}
-				}
+					},
+				},
 			],
 			data: [],
 			total: 0,
-			statusCodes: []
+			statusCodes: [],
 		};
 	},
 
@@ -300,7 +330,7 @@ export default {
 			}
 		},
 		getStatusCodes() {
-			getAllotStatus().then(res => {
+			getAllotStatus().then((res) => {
 				if (res.success) {
 					this.statusCodes = res.result;
 				}
@@ -330,7 +360,7 @@ export default {
 		getDataList() {
 			this.loading = true;
 			getAllotList(this.globalUtil.filterParams(this.searchForm)).then(
-				res => {
+				(res) => {
 					this.loading = false;
 					if (res.success) {
 						this.data = res.result.content;
@@ -389,14 +419,14 @@ export default {
 				content: '您确认要删除调拨单' + v.allotId + ' ?',
 				loading: true,
 				onOk: () => {
-					deleteAllot(v.allotId).then(res => {
+					deleteAllot(v.allotId).then((res) => {
 						this.$Modal.remove();
 						if (res.success) {
 							this.$Msg.success('操作成功');
 							this.getDataList();
 						}
 					});
-				}
+				},
 			});
 		},
 		//流程提交
@@ -415,7 +445,7 @@ export default {
 							'1005',
 							this.selectCount
 						);
-					}
+					},
 				});
 			} else {
 				this.$Msg.warning('请勿重复提交流程');
@@ -433,10 +463,10 @@ export default {
 			} else {
 				this.$Msg.warning('草稿数据无流程记录');
 			}
-		}
+		},
 	},
 	mounted() {
 		this.init();
-	}
+	},
 };
 </script>
